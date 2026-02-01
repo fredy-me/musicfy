76808492613131c2f84fe5ef2851f04fbded7327
fredy-me <fredymelkizedek@gmail.com>
2026-02-01 23:25:41 +0300

    Fix error reporting and improve code readability in index.php

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include "config/db.php";
+
 $singers = mysqli_query($conn, "SELECT * FROM singers");
 ?>
 
 <link rel="stylesheet" href="assets/css/style.css">
 
 <div class="grid">
-<?php while($s = mysqli_fetch_assoc($singers)) { ?>
-  <a href="pages/singer.php?id=<?= $s['id'] ?>" class="card">
-    <img src="assets/images/singers/<?= $s['image'] ?>">
-    <h3><?= $s['name'] ?></h3>
+<?php while ($s = mysqli_fetch_assoc($singers)) { ?>
+  <a href="pages/singer.php?id=<?php echo $s['id']; ?>" class="card">
+    <img src="assets/images/singers/<?php echo $s['image']; ?>">
+    <h3><?php echo $s['name']; ?></h3>
   </a>
 <?php } ?>
 </div>
